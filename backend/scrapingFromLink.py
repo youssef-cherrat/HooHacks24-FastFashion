0c6a36b59390501e184f0f9d0aa936c48ff2849c
acai16 <alan.cai.work@gmail.com>
2024-03-23 14:17:15 -0400

    new webscrape

--- backend/scrapingFromLink.py
+++ backend/scrapingFromLink.py
@@ -2,9 +2,44 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'  }
-url='https://www.zara.com/us/en/woman-dresses-l1066.html?v1=2113500&regionGroupId=1&page={page}'
-r=requests.get(url,headers=headers)
-soup=BeautifulSoup(r.content,'lxml')
-name=soup.find_all('h3')
-price=soup.find_all('span',class_='money-amount__main')
+baseurl = "https://www.thewhiskyexchange.com"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+
+
+k = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky').text
+soup=BeautifulSoup(k,'html.parser')
+productlist = soup.find_all("li",{"class":"product-grid__item"})
+print(productlist)
+
+productlinks = ["https://www.thewhiskyexchange.com/p/36362/suntory-toki"]
+
+
+
+data=[]
+for link in productlinks:
+    f = requests.get(link,headers=headers).text
+    hun=BeautifulSoup(f,'html.parser')
+
+    try:
+        price=hun.find("p",{"class":"product-action__price"}).text.replace('\n',"")
+    except:
+        price = None
+
+    try:
+        about=hun.find("div",{"class":"product-main__description"}).text.replace('\n',"")
+    except:
+        about=None
+
+    try:
+        rating = hun.find("div",{"class":"review-overview"}).text.replace('\n',"")
+    except:
+        rating=None
+
+    try:
+        name=hun.find("h1",{"class":"product-main__name"}).text.replace('\n',"")
+    except:
+        name=None
+
+    whisky = {"name":name,"price":price,"rating":rating,"about":about}
+
+    data.append(whisky)
