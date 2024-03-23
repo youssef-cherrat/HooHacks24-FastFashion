e69743270491d8648763a2c5da9efb14d42135ea
youssef-cherrat <115671884+youssef-cherrat@users.noreply.github.com>
2024-03-23 13:45:30 -0400

    add search functionality with depop ebay mercari psohmark

--- backend/example.py
+++ backend/example.py
@@ -2,20 +2,30 @@
 from serpapi import GoogleSearch
 import json
 
-def write_results_to_file(results):
+def write_results_to_file(search_term):
+    # appending necessary platforms to search query
+    full_search_query = f"{search_term} ebay depop mercari poshmark"
+    
+    #set up parameters for api
+    
+    params = {
+      "engine": "google_shopping",
+      "q": full_search_query,
+      "location": "United States",
+      "api_key": "143843bcced7c5448ca482aca06a7ae38cba7959463237e78e2591b92133756a"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    # write results to file
     with open('results.json', 'w') as f:
         json.dump(results, f, indent=4)
+    print("Results written to file")
 
-params = {
-  "engine": "google_shopping",
-  "q": "black shirt ebay depop mercari poshmark",
-  "location": "United States",
-  "api_key": "143843bcced7c5448ca482aca06a7ae38cba7959463237e78e2591b92133756a"
-}
+def test_search():
+    search_term = input("Enter search term: ")
+    write_results_to_file(search_term)
 
-
-search = GoogleSearch(params)
-results = search.get_dict()
-
-write_results_to_file(results)
-print("Results saved to results.json")
+if __name__ == "__main__":
+    test_search()
