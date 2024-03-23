fa385fb3afa78eab7fd746c9233d33a7f625b33c
youssef-cherrat <115671884+youssef-cherrat@users.noreply.github.com>
2024-03-23 13:31:55 -0400

    framework for backend search using text base

--- backend/example.py
+++ backend/example.py
@@ -1,14 +1,21 @@
 
 from serpapi import GoogleSearch
+import json
+
+def write_results_to_file(results):
+    with open('results.json', 'w') as f:
+        json.dump(results, f, indent=4)
 
 params = {
-  "engine": "google_images",
-  "q": "black shirt ebay",
+  "engine": "google_shopping",
+  "q": "black shirt ebay depop mercari poshmark",
   "location": "United States",
   "api_key": "143843bcced7c5448ca482aca06a7ae38cba7959463237e78e2591b92133756a"
 }
 
+
 search = GoogleSearch(params)
 results = search.get_dict()
 
-print(results)
+write_results_to_file(results)
+print("Results saved to results.json")
