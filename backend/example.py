52e6cc607cdf2b4a1c844bf45992df39eb61b620
acai16 <alan.cai.work@gmail.com>
2024-03-23 13:09:58 -0400

    text based

--- backend/example.py
+++ backend/example.py
@@ -1,34 +1,14 @@
-import requests
 
-def reverse_image_search(image_url):
-    api_key = "143843bcced7c5448ca482aca06a7ae38cba7959463237e78e2591b92133756a"
-    search_url = "https://serpapi.com/search"
-    params = {
-        "engine": "google_reverse_image",
-        "image_url": image_url,
-        "api_key": api_key
-    }
+from serpapi import GoogleSearch
 
-    response = requests.get(search_url, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return "Error: " + response.text
+params = {
+  "engine": "google_images",
+  "q": "black shirt ebay",
+  "location": "United States",
+  "api_key": "143843bcced7c5448ca482aca06a7ae38cba7959463237e78e2591b92133756a"
+}
 
-def filter_results(results):
-    filtered_links = []
-    domains = ["mercari.com", "poshmark.com", "ebay.com", "grailed.com", "depop.com"]
-    for image in results.get("images_results", []):
-        link = image.get("link", "")
-        if any(domain in link for domain in domains):
-            filtered_links.append(link)
-    return filtered_links
+search = GoogleSearch(params)
+results = search.get_dict()
 
-if __name__ == "__main__":
-    image_url = "URL_OF_THE_IMAGE_YOU_WANT_TO_SEARCH"
-    results = reverse_image_search(image_url)
-    if isinstance(results, dict):
-        filtered_links = filter_results(results)
-        print(filtered_links)
-    else:
-        print(results)
+print(results)
