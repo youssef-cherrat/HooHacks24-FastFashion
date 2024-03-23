73f8fd1743e3207d6388e8e2ebfcb4411350e0b9
Peter Do <pdoo2004@vt.edu>
2024-03-23 14:48:16 -0400

    About Page

--- hoohacks/src/App.js
+++ hoohacks/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar'; // Adjust the path as necessary
 // import { BannerSearch } from './components/BannerSearch'; // Comment out
 // import { ResultCards } from './components/ResultCards'; // Comment out
-// import About from "./pages/About"; // Comment out
+import About from "./pages/About"; // Comment out
 import './App.css';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={
             <>
-              {/* <About /> */}
+              <About />
             </>
           } />
           {/* Temporarily remove other routes to isolate the issue */}
