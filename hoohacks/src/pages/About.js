44cc84bdef22a4154274c56c4c4d05689f2dd51a
Ethan Do <ethando@gmail.com>
2024-03-23 22:19:28 -0400

    fixed about

--- hoohacks/src/pages/About.js
+++ hoohacks/src/pages/About.js
@@ -10,8 +10,19 @@ const About = () => {
         <div className="row">
           <div className="col-12 col-md-6 d-flex align-items-center">
             <div>
-              <h1 className="mb-3">The GreenThreads Story</h1>
-              <p className="lead">GreenThreads is a forward-thinking initiative transforming the fashion landscape by championing sustainability. Our mission is to curtail the effects of fast fashion by connecting eco-conscious consumers with small, local businesses and individual sellers who offer a treasure trove of second-hand clothing. We're not a retailer; we're a gateway to a world of environmentally friendly fashion choices, where every purchase supports the longevity of apparel and nurtures community-based commerce. Embrace style with substance and join GreenThreads in weaving a future where fashion is kind to our planet.</p>
+              <h1 className="mb-4 title">The GreenThreads Story</h1>
+              <p className="lead">
+                GreenThreads is a forward-thinking initiative transforming the
+                fashion landscape by championing sustainability. Our mission is
+                to curtail the effects of fast fashion by connecting
+                eco-conscious consumers with small, local businesses and
+                individual sellers who offer a treasure trove of second-hand
+                clothing. We're not a retailer; we're a gateway to a world of
+                environmentally friendly fashion choices, where every purchase
+                supports the longevity of apparel and nurtures community-based
+                commerce. Embrace style with substance and join GreenThreads in
+                weaving a future where fashion is kind to our planet.
+              </p>
             </div>
           </div>
           <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
