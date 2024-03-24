87f2af627da5b933df44297ebddd4dab96ab5309
Ethan Do <ethando@gmail.com>
2024-03-24 00:53:16 -0400

    email form

--- hoohacks/src/components/SearchBar.js
+++ hoohacks/src/components/SearchBar.js
@@ -12,7 +12,7 @@ export const SearchBar = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch('http://localhost:5000/search', { // Update the URL/port as needed
+    fetch('http://127.0.0.1:5000/search', { // Update the URL/port as needed
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
