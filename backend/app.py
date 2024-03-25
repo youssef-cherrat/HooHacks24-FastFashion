f69dbe0eae6b066d4932d3e10ebd8f8c7a184969
youssef-cherrat <115671884+youssef-cherrat@users.noreply.github.com>
2024-03-25 18:29:44 -0400

    configuration

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import writer
 import os
 
@@ -7,6 +7,7 @@ app = Flask(__name__, static_folder='../hoohacks/build', static_url_path='/')
 CORS(app)  # Allow cross-origin requests (needed for local development)
 
 @app.route('/search', methods=['POST'])
+@cross_origin()
 def handle_search():
     data = request.json
     url = data.get('url')
@@ -19,12 +20,9 @@ def handle_search():
     else:
         return jsonify({"error": "URL not provided"}), 400
     
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+@app.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 if __name__ == '__main__':
     app.run(debug=True)
